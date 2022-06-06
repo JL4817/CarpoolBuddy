425514bee6a548eb8c6df05c4b0035d458684889
JL4817 <89381622+JL4817@users.noreply.github.com>
2022-06-06 15:09:18 +0200

    it works now need creative task

--- app/src/main/java/com/example/carpoolbuddy/Controllers/CreateVehicle.java
+++ app/src/main/java/com/example/carpoolbuddy/Controllers/CreateVehicle.java
@@ -35,6 +35,7 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.ArrayList;
@@ -231,6 +232,9 @@ public class CreateVehicle extends AppCompatActivity {
 
         //add the new vehicle to the database
         newSignUpKey.set(newVehicle);
+
+        firestore.collection(Constants.PEOPLE_COLLECTION).document(mUser.getUid())
+                .update("ownedVehicles", FieldValue.arrayUnion(vehicleKey));
 
     }
 
